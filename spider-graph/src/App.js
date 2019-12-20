943f2187654ec3776f0ca9fc6513e52748536640
bsherwood9 <36565536+bsherwood9@users.noreply.github.com>
2019-12-19 20:11:13 -0700

    Making the changes to graph. Figuring out the logic.

--- spider-graph/src/App.js
+++ spider-graph/src/App.js
@@ -1,24 +1,66 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { useState, useEffect } from "react";
+import logo from "./logo.svg";
+import SpiderChart from "./components/SpiderChart.js";
+import CreateChart from "./components/CreateChart";
+import Dataset from "./components/Dataset";
+import "./App.css";
 
 function App() {
+  const [formData, setFormData] = useState({ title: "", axe: "" });
+  const [axis, setAxis] = useState([]);
+  const [title, setTitle] = useState("This is a Radar Example");
+  const [label, setLabel] = useState("Custom Label");
+  const [numbers, setNumbers] = useState([]);
+  const [datasets, setDatasets] = useState([
+    // {
+    //   label: "My First dataset",
+    //   backgroundColor: "rgba(255, 87, 51,0.2)",
+    //   pointBackgroundColor: "rgba(220,220,220,1)",
+    //   data: [10, 20, 30, 2]
+    // }
+    // {
+    //   label: "Hidden dataset",
+    //   backgroundColor: "rgba(187, 51, 255 ,0.2)",
+    //   data: [12, 19, 3, 5]
+    // },
+    // {
+    //   label: "My Second dataset",
+    //   backgroundColor: "rgba(255, 255, 51,0.2)",
+    //   pointBackgroundColor: "rgba(151,187,205,1)",
+    //   data: [16, 10, 15, 14]
+    // }
+  ]);
+
+  const addToDatasets = event => {
+    event.preventDefault();
+    console.log("Before", datasets);
+    setDatasets([...datasets, { label: label, data: [...numbers, numbers] }]);
+
+    console.log("After", datasets);
+  };
+
+  useEffect(() => {
+    console.log("Axis", axis);
+  }, [axis]);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <SpiderChart axis={axis} title={title} datasets={datasets} />
+      <CreateChart
+        setAxis={setAxis}
+        setTitle={setTitle}
+        axis={axis}
+        setFormData={setFormData}
+        formData={formData}
+      />
+      <Dataset
+        axis={axis}
+        addToDatasets={addToDatasets}
+        setLabel={setLabel}
+        label={label}
+        setNumbers={setNumbers}
+        numbers={numbers}
+      />
     </div>
   );
 }
