89613871360cea22fc349f982517a17e0f5d4bbd
Westley Feller <feller.westley@gmail.com>
2019-12-22 16:57:25 -0800

    Demo of what the graph could look like

--- src/app/theme.js
+++ src/app/theme.js
@@ -3,6 +3,13 @@ import { createMuiTheme, responsiveFontSizes } from '@material-ui/core/styles';
 import { blueGrey, deepOrange } from '@material-ui/core/colors';
 
 const theme = createMuiTheme({
+  props: {
+    MuiTypography: {
+      variantMapping: {
+        body1: 'p',
+      },
+    },
+  },
   typography: {
     h1: {
       fontFamily: "'Quicksand', sans-serif",
