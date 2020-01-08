1498cdfe763add9b71dfe21105bad0ce48e75d65
Westley Feller <feller.westley@gmail.com>
2020-01-07 20:08:53 -0800

    Removed step size

--- src/features/graph/ViewGraph.jsx
+++ src/features/graph/ViewGraph.jsx
@@ -84,7 +84,6 @@ const ViewGraph = () => {
 
       ticks: {
         min: 0,
-        stepSize: 0.5,
         fontColor: theme.palette.text.secondary,
         fontFamily: theme.typography.caption.fontFamily,
         fontSize: 0.75 * theme.typography.fontSize,
