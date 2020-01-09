56865e71ce4ce660cdcb54c947c1cc3b6d016a6d
bsherwood9 <36565536+bsherwood9@users.noreply.github.com>
2020-01-08 21:22:25 -0700

    Adding some styling

--- src/app/Brett/Profile.js
+++ src/app/Brett/Profile.js
@@ -1,12 +1,33 @@
-import React, { useEffect, useState } from 'react';
-import styled from 'styled-components';
+import React from 'react';
+import { useEffect, useState } from 'react';
 import axiosWithAuth from '../../api/utils/axiosWithAuth';
+import {
+  makeStyles,
+  Box,
+  Container,
+  Paper,
+  Typography,
+  Button,
+} from '@material-ui/core';
 
-const Div = styled.div`
-  margin-top: 10%;
-`;
+const useStyles = makeStyles({
+  title: {
+    flexGrow: 1,
+    padding: '1rem',
+  },
+  textArea: {
+    margin: '.5rem 10%',
+    display: 'flex',
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    justifyContent: 'space-between',
+  },
+});
+
 const Profile = () => {
   const [userData, setUserData] = useState({});
+  const classes = useStyles();
+
   useEffect(() => {
     axiosWithAuth()
       .get(`users/${localStorage.getItem('userId')}`)
@@ -22,12 +43,27 @@ const Profile = () => {
     console.log(userData);
   }, [userData]);
   return (
-    <Div>
-      <h1>Username: {userData.username}</h1>
-      <h1>Name: {userData.name}</h1>
-      <h1>Email: {userData.email}</h1>
-      <h1>Password: {userData.password}</h1>
-    </Div>
+    <Container maxWidth="sm">
+      <Paper elevation={1}>
+        <Box component="div" mt={18} py="1.5rem">
+          <Typography variant="h3" align="center" className={classes.title}>
+            User Information
+          </Typography>
+          <Box className={classes.textArea}>
+            <Typography variant="h6">Username: {userData.username}</Typography>
+            <Button>Edit</Button>
+          </Box>
+          <Box className={classes.textArea}>
+            <Typography variant="h6">Name: {userData.name}</Typography>
+            <Button>Edit</Button>
+          </Box>
+          <Box className={classes.textArea}>
+            <Typography variant="h6">Email: {userData.email}</Typography>
+            <Button>Edit</Button>
+          </Box>
+        </Box>
+      </Paper>
+    </Container>
   );
 };
 
